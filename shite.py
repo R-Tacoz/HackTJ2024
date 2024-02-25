c83eed51816661ac8adf586d942128e470cb1683
R-Tacoz <roccozxyt@gmail.com>
2024-02-25 07:18:08 -0500

    check

--- shite.py
+++ shite.py
@@ -1,45 +1,35 @@
+# Importing standard Qiskit libraries
 from qiskit import QuantumCircuit, transpile
 # from qiskit.tools.jupyter import *
 from qiskit.visualization import *
 # from ibm_quantum_widgets import *
+
+# qiskit-ibmq-provider has been deprecated.
+# Please see the Migration Guides in https://ibm.biz/provider_migration_guide for more detail.
 # from qiskit_ibm_runtime import QiskitRuntimeService, Sampler, Estimator, Session, Options
 
 # from qiskit import IBMQ, Aer
-from qiskit.primitives.sampler import Sampler
-# from qiskit_aer.primitives.sampler import Sampler
+from qiskit_aer.primitives.sampler import Sampler
+
+# Loading your IBM Quantum account(s)
 # service = QiskitRuntimeService(channel="ibm_quantum")
+
+# Invoke a primitive. For more details see https://docs.quantum-computing.ibm.com/run/primitives
+# result = Sampler().run(circuits).result()
+
+
+
+#imports
 import time
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 from qiskit_algorithms import AmplificationProblem, Grover
 from qiskit.circuit.library import PhaseOracle
 from qiskit.primitives import BackendSampler
 # from qiskit.tools.visualization import plot_histogram
-
-
-def find_sat():
-    start_time = time.perf_counter()
-    oracle = PhaseOracle.from_dimacs_file('tempCNF.cnf')
-    problem = AmplificationProblem(oracle)
-    sampler = Sampler()
-    num_sols= 1
-    while(num_sols <= 2**9): #If the maximal doesn't work, double the num_solutions parameter and try again.
-        iterations = Grover.optimal_num_iterations(num_solutions=num_sols, num_qubits=oracle.num_qubits)
-        grover = Grover(iterations=iterations, sampler=sampler)
-        result = grover.amplify(problem)
-        
-        mes = result.top_measurement
-        if oracle.evaluate_bitstring(mes):
-            # display(plot_histogram(result.circuit_results[0]))
-            tot_time = time.perf_counter() - start_time
-            return result.oracle_evaluation, tot_time
-        num_sols*=2
-    
-    tot_time = time.perf_counter() - start_time
-    
-    return result.oracle_evaluation, tot_time
-
 def generate_dimacs_cnf(n):
      # Define the number of boolean variables
     num_variables = 9
@@ -67,5 +57,34 @@ def generate_dimacs_cnf(n):
                 file.write(' '.join(map(str, clause)) + ' 0\n')
                 clauses.add(tuple(clause))
                 
-generate_dimacs_cnf(4)
+            #find_sat method
+
+#This method reads information from a CNF file 'tempCNF.cnf' and runs Grover's 
+#Algorithm The output is a dictionary 'results' that contains all of the boolean
+#states and their measurement outcomes.
+
+
+
+def find_sat():
+    start_time = time.perf_counter()
+    oracle = PhaseOracle.from_dimacs_file('tempCNF.cnf')
+    problem = AmplificationProblem(oracle)
+    sampler = Sampler()
+    num_sols= 1
+    while(num_sols <= 2**9): #If the maximal doesn't work, double the num_solutions parameter and try again.
+        iterations = Grover.optimal_num_iterations(num_solutions=num_sols, num_qubits=oracle.num_qubits)
+        grover = Grover(iterations=iterations, sampler=sampler)
+        result = grover.amplify(problem)
+        
+        mes = result.top_measurement
+        if oracle.evaluate_bitstring(mes):
+            # display(plot_histogram(result.circuit_results[0]))
+            tot_time = time.perf_counter() - start_time
+            return result.oracle_evaluation, tot_time
+        num_sols*=2
+    
+    tot_time = time.perf_counter() - start_time
+    
+    return result.oracle_evaluation, tot_time
+
 find_sat()
